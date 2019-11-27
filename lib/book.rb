ad80283ef007e3e49b73bd9870250e890b790b30
Jamal Rjoul <jamensky1991@gmail.com>
2019-11-27 19:46:28 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,37 @@
 
 
+class Book
+  #attr_reader :title, :author, :page_count, :genre
+  def initialize(title)
+    @title = title
+  end
+
+  def author=(author)
+    @author = author
+  end
+
+  def author
+    @author
+  end
+
+  def page_count=(page_count)
+    @page_count = page_count
+  end
+
+  def page_count
+    @page_count
+  end
+
+  def genre=(genre)
+    @genre = genre
+  end
+
+  def genre
+    @genre
+  end
+
+  #def turn_page
+end
+
+book_1 = Book.new(title)
+book_1.title = "And Then There Were None"
