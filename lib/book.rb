5d76b176108392e02dbe759b9787b6916a2b18d3
Jamal Rjoul <jamensky1991@gmail.com>
2019-11-27 19:53:45 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -38,10 +38,10 @@ class Book
  attr_accessor :title, :author, :page_count, :genre
 
  def initialize(title, author, page_count, genre)
-   @title =
-   @author =
-   @page_count =
-   @genre =
+   @title = title 
+   @author = author
+   @page_count = page_count
+   @genre = genre
 end
 
 
